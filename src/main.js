ea9f982f1a487b2f71b5974f55925f4ddbb510d5
Dapo Ola-Olatunji <71828782+Nehemiah01@users.noreply.github.com>
2023-08-08 01:17:17 +0100

    fixed styling issue

--- src/main.js
+++ src/main.js
@@ -2,11 +2,12 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
-if (import.meta.env.MODE === 'development') {
-    import('../style.css')
-} else {
-    import('../build.css')
-}
+import('../style.css')
+// if (import.meta.env.MODE === 'development') {
+//     import('../style.css')
+// } else {
+//     import('../build.css')
+// }
 
 createApp(App).use(router).mount('#app')
 
